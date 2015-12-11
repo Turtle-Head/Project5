f39a97fb2c51b9d573215df9e75c9456508b2349
Turtle-Head <djinn.jecture@gmail.com>
2015-12-10 21:09:34 -0600

    Added Wiki Import and Map Functions as well as a view and model variable to store page modifications and data from the import functions

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // yelp import //
-var yelp = {
+var ViewModel = {
   nonce_generate: function(){
     var length = 5+(Math.floor(Math.random()*32));
     // SRC #002{'https://blog.nraboy.com/2015/03/create-a-random-nonce-string-using-javascript/'}
@@ -15,14 +15,14 @@ var yelp = {
     var yelp_url=YELP_BASE_URL;
     // Set up OAuth to authenticate request
     /**
-     * Generates a random number and returns it as a string for OAuthentication
-     * @return {string}
-     */
+    * Generates a random number and returns it as a string for OAuthentication
+    * @return {string}
+    */
 
     var parameters = {
       oauth_consumer_key: YELP_KEY,
       oauth_token: YELP_TOKEN,
-      oauth_nonce: yelp.nonce_generate(),
+      oauth_nonce: ViewModel.nonce_generate(),
       oauth_timestamp: Math.floor(Date.now()/1000),
       oauth_signature_method: 'HMAC-SHA1',
       oauth_version : '1.0',
@@ -42,16 +42,15 @@ var yelp = {
       cache: true,                // This is crucial to include as well to prevent jQuery from adding on a cache-buster parameter "_=23489489749837", invalidating our oauth-signature
       dataType: 'jsonp',
       success: function(results) {
-      // Do stuff with results
+        // Do stuff with results
         console.log(results);
 
       },
       error: function() {
-      // Do stuff on fail
+        // Do stuff on fail
         console.log('Failed to import Data');
       }
     };
-
     // Send AJAX query via jQuery library.
     $.ajax(settings);
     // End of source #001
@@ -66,7 +65,135 @@ var yelp = {
     searchTerm = searchTerm.replace("++", "+");
     // Return searchTerm for use with the importYelp function
     return(searchTerm);
+  },
+  gMap: function(obj){
+      var locations;
+      var mapOptions = {
+        disableDefaultUI: true,
+        noClear: true
+      };
+      map = new google.maps.Map(document.querySelector('#map'), mapOptions);
+      /*
+      locationFinder() returns an array of every location string from the JSONs
+      written for bio, education, and work.
+      */
+      function locationFinder() {
+        // initializes an empty array
+        var locations = [];
+        // adds the single location property from model to the locations array
+        locations.push(obj./*('some json variable')[i].*/location);
+        return locations;
+      }
+      /*
+      createMapMarker(placeData) reads Google Places search results to create map pins.
+      placeData is the object returned from search results containing information
+      about a single location.
+      */
+      function createMapMarker(placeData) {
+        // The next lines save location data from the search result object to local variables
+        var lat = placeData.geometry.location.lat();  // latitude from the place service
+        var lon = placeData.geometry.location.lng();  // longitude from the place service
+        var name = placeData.formatted_address;   // name of the place from the place service
+        var bounds = window.mapBounds;            // current boundaries of the map window
+        // marker is an object with additional data about the pin for a single location
+        var marker = new google.maps.Marker({
+          map: map,
+          position: placeData.geometry.location,
+          title: name
+        });
+        // infoWindows are the little helper windows that open when you click
+        // or hover over a pin on a map. They usually contain more information
+        // about a location.
+        var infoWindow = new google.maps.InfoWindow({
+          content: name
+        });
+        // hmmmm, I wonder what this is about...
+        google.maps.event.addListener(marker, 'click', function() {
+          // your code goes here!
+          infoWindow.open(map,marker);
+        });
+        // this is where the pin actually gets added to the map.
+        // bounds.extend() takes in a map location object
+        bounds.extend(new google.maps.LatLng(lat, lon));
+        // fit the map to the new marker
+        map.fitBounds(bounds);
+        // center the map
+        map.setCenter(bounds.getCenter());
+      }
+      /*
+      callback(results, status) makes sure the search returned results for a location.
+      If so, it creates a new map marker for that location.
+      */
+      function callback(results, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+          createMapMarker(results[0]);
+        }
+      }
+      /*
+      pinPoster(locations) takes in the array of locations created by locationFinder()
+      and fires off Google place searches for each location
+      */
+      function pinPoster(locations) {
+        // creates a Google place search service object. PlacesService does the work of
+        // actually searching for location data.
+        var service = new google.maps.places.PlacesService(map);
+        // Iterates through the array of locations, creates a search object for each location
+        for (var place in locations) {
+          // the search request object
+          var request = {
+            query: locations[place]
+          };
+          // Actually searches the Google Maps API for location data and runs the callback
+          // function with the search results after each search.
+          service.textSearch(request, callback);
+        }
+      }
+      // Sets the boundaries of the map based on pin locations
+      window.mapBounds = new google.maps.LatLngBounds();
+      // locations is an array of location strings returned from locationFinder()
+      locations = locationFinder();
+      // pinPoster(locations) creates pins on the map for each location in
+      // the locations array
+      pinPoster(locations);
+  },
+  importWiki: function(e){
+    var wikiURL = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + e + '&format=json&callback=wikiCallback';
+    var wikiRequestTimeout = setTimeout(function(){
+      console.log("Failed to get wikipedia resources");
+    }, 8000);
+
+    $.ajax({
+      url: wikiURL,
+      dataType: "jsonp",
+      //jsonp: "callback",
+      success: function( response ){
+        // Writes Wiki articles out to page
+        var articleList = response[1];
+        // Do stuff with the Wiki import
+        console.log(articleList);
+        // Clear the timeout counter
+        clearTimeout(wikiRequestTimeout);
+      }
+    });
+  },
+
+};
+
+var Model = {
+  yelpData:{
+
+  },
+  mapData:{
+
+  },
+  wikiData:{
+
   }
 };
 
-yelp.importYelp(yelp.searchString('art entertainment'),yelp.searchString('the forks winnipeg'));
+var View = {
+
+};
+
+// Yelp Import Call Needs to be moved to a more appropriate location
+//yelp.importYelp(yelp.searchString('art entertainment'),yelp.searchString('the forks winnipeg'));
